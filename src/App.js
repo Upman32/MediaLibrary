6c174f211168ab7883a149a5421bf6cf53fffc17
Kirill Seredin <k.seredin@rambler.ru>
2022-02-22 21:37:42 +0200

    Create app and set up eslint

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/components/Content.jsx
@@ -0,0 +1,10 @@
+
+const Content = () => {
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default Content
