5291fb13fa7baab57fdfdf30ce60f05487e56e24
Kirill Seredin <k.seredin@rambler.ru>
2022-02-22 21:43:10 +0200

    remove react presets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React from 'react'
-import './App.css'
 
 const App = () => {
   return (
     <div className="App">
-      <div>
-        <div></div>
-      </div>
+     hello world
     </div>
   )
 }
